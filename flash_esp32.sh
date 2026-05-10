842be6b4d785a19a60fa7100d50aaf35eea046f8
hdoanf <Bestbubuom@gmail.com>
2026-05-11 00:17:44 +0700

    chore: remove build artifacts from remote but keep local

--- flash_esp32.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Starting ESP32-C3 Flash Process..."
-sudo /home/doanchim/.arduino15/packages/esp32/tools/esptool_py/5.2.0/esptool --chip esp32c3 --port /dev/ttyACM0 --baud 460800 write-flash 0x0 /home/doanchim/doantotnhiep/doantotnghiep/build_esp32/health_esp32.ino.merged.bin
-echo "Done! Please reset your ESP32."
